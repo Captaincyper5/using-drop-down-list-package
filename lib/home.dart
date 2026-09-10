911e4206fbc092bd1a13db91c319c3dabae60257
Captaincyper5 <salemaldmyry66xd@gmailcom>
2026-09-10 22:17:01 +0200

    new commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,55 +1,99 @@
 import 'package:flutter/material.dart';
-import 'package:app2/searchdelegate.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
-
   @override
   State<Home> createState() => _MyState();
 }
 
 class _MyState extends State<Home> {
-  // استخدام القائمة للتحكم في الأبعاد بأمان
-  final List<double> cardWidths = [200.0, 300.0];
-  final List title = ["to search click the icon", "اضغط على الايقونة للبحث"];
+  late ScrollController scrollx;
+  @override
+  void dispose() {
+    scrollx.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    scrollx = ScrollController();
+    scrollx.addListener(() {
+      print("${scrollx.offset}");
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('welcome                    مرحبا بك'),
-        foregroundColor: Colors.white,
+        title: Text(
+          'Welcome                          مرحبا بك',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
         backgroundColor: Colors.blue,
-        actions: [
-          IconButton(
-            onPressed: () {
-              showSearch(context: context, delegate: Custom());
-            },
-            icon: const Icon(
-              Icons.search,
-            ), // تغيير الأيقونة إلى بحث لتكون أوضح للمستخدم
-          ),
-        ],
+        foregroundColor: Colors.white,
       ),
-      body: Stack(
+      body: ListView(
+        controller: scrollx,
+        clipBehavior: Clip.none,
         children: [
-          Positioned(
-            top: 250,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            child: ListView.builder(
-              itemCount: title.length,
-              itemBuilder: (context, i) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(title[i], textAlign: TextAlign.center),
-                    SizedBox(width: 10),
-                    Icon(Icons.search),
-                  ],
-                );
-              },
+          MaterialButton(
+            onPressed: () {
+              scrollx.animateTo(
+                906,
+                duration: Duration(seconds: 5),
+                curve: Curves.ease,
+              );
+            },
+            child: Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: const Color(0xFF605757),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Icon(Icons.arrow_downward, color: Colors.white),
+            ),
+          ),
+          ...List.generate(
+            30,
+            (index) => Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: index.isEven
+                      ? [Colors.amber, Colors.blue]
+                      : [Colors.purple, Colors.green],
+                ),
+              ),
+              child: Text(
+                "$index",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+          MaterialButton(
+            onPressed: () {
+              scrollx.animateTo(
+                0,
+                duration: Duration(seconds: 5),
+                curve: Curves.ease,
+              );
+            },
+            child: Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: const Color(0xFF605757),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Icon(Icons.arrow_upward, color: Colors.white),
             ),
           ),
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,17 @@ import 'package:app2/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
   @override
-  State<MyApp> createState() => _MyState();
+  State<MyApp> createState()=> _MyState();
 }
-
 class _MyState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
+    return MaterialApp(debugShowCheckedModeBanner: false,home: Home());
   }
 }
