7f6f9710531f00998ad46144905b286c744c0e01
Captaincyper5 <salemaldmyry66xd@gmailcom>
2026-09-11 14:54:57 +0200

    new commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.app2"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -7,93 +8,41 @@ class Home extends StatefulWidget {
 }
 
 class _MyState extends State<Home> {
-  late ScrollController scrollx;
-  @override
-  void dispose() {
-    scrollx.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    scrollx = ScrollController();
-    scrollx.addListener(() {
-      print("${scrollx.offset}");
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.redAccent,
       appBar: AppBar(
-        title: Text(
-          'Welcome                          مرحبا بك',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
+        title: Text('Welcome in my app'),
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
       body: ListView(
-        controller: scrollx,
-        clipBehavior: Clip.none,
         children: [
-          MaterialButton(
-            onPressed: () {
-              scrollx.animateTo(
-                906,
-                duration: Duration(seconds: 5),
-                curve: Curves.ease,
-              );
-            },
-            child: Container(
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: const Color(0xFF605757),
-                borderRadius: BorderRadius.circular(25),
+          SizedBox(height: 250),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 130),
+            padding: EdgeInsets.all(10),
+            child: MaterialButton(
+              color: Colors.black,
+              textColor: Colors.white,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
               ),
-              child: Icon(Icons.arrow_downward, color: Colors.white),
-            ),
-          ),
-          ...List.generate(
-            30,
-            (index) => Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: index.isEven
-                      ? [Colors.amber, Colors.blue]
-                      : [Colors.purple, Colors.green],
-                ),
-              ),
-              child: Text(
-                "$index",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ),
-          MaterialButton(
-            onPressed: () {
-              scrollx.animateTo(
-                0,
-                duration: Duration(seconds: 5),
-                curve: Curves.ease,
-              );
-            },
-            child: Container(
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: const Color(0xFF605757),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: Icon(Icons.arrow_upward, color: Colors.white),
+              onPressed: () {
+                AwesomeDialog(
+                  context: context,
+                  dialogType: DialogType.info,
+                  animType: AnimType.leftSlide,
+                  title: "dialog",
+                  desc: "click",
+                  btnCancelOnPress: () {},
+                  btnCancelIcon: Icons.cancel,
+                  btnOkOnPress: () {},
+                  btnOkIcon: Icons.check_circle,
+                )..show();
+              },
+              child: Text("click"),
             ),
           ),
         ],
