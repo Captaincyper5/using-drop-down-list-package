1662a406b2382e09d2f45312a4dd2e532d650ff7
Captaincyper5 <salemaldmyry66xd@gmailcom>
2026-09-12 13:26:16 +0200

    new commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
-import 'package:awesome_dialog/awesome_dialog.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,41 +10,49 @@ class Home extends StatefulWidget {
 }
 
 class _MyState extends State<Home> {
+  List data = [];
+  bool loading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.redAccent,
       appBar: AppBar(
-        title: Text('Welcome in my app'),
+        title: Text('My App'),
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
       body: ListView(
         children: [
-          SizedBox(height: 250),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 130),
-            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            height: 40,
+            margin: EdgeInsets.symmetric(horizontal: 100, vertical: 10),
             child: MaterialButton(
-              color: Colors.black,
-              textColor: Colors.white,
-              shape: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              onPressed: () {
-                AwesomeDialog(
-                  context: context,
-                  dialogType: DialogType.info,
-                  animType: AnimType.leftSlide,
-                  title: "dialog",
-                  desc: "click",
-                  btnCancelOnPress: () {},
-                  btnCancelIcon: Icons.cancel,
-                  btnOkOnPress: () {},
-                  btnOkIcon: Icons.check_circle,
-                )..show();
+              onPressed: () async {
+                loading = true;
+                setState(() {});
+                var response = await get(
+                  Uri.parse("https://jsonplaceholder.org/posts"),
+                );
+                var responsex = jsonDecode(response.body);
+                data.addAll(responsex);
+                print(responsex);
+                loading = false;
+                setState(() {});
               },
-              child: Text("click"),
+              child: Text("request", style: TextStyle(color: Colors.white)),
+            ),
+          ),
+          if (loading) Center(child: CircularProgressIndicator()),
+          ...List.generate(
+            data.length,
+            (index) => Card(
+              child: ListTile(
+                title: Text("${data[index]["title"]}"),
+                subtitle: Text("${data[index]["content"]}"),
+              ),
             ),
           ),
         ],
