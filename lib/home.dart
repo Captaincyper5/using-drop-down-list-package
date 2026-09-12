ef3dabd3046089015799aefe7f0ab1ee3a6cb4ca
Captaincyper5 <salemaldmyry66xd@gmailcom>
2026-09-12 13:45:47 +0200

    new commit

--- lib/home.dart
+++ lib/home.dart
@@ -11,7 +11,22 @@ class Home extends StatefulWidget {
 
 class _MyState extends State<Home> {
   List data = [];
-  bool loading = false;
+  bool loading = true;
+  api() async {
+    var response = await get(Uri.parse("https://jsonplaceholder.org/posts"));
+    var responsex = jsonDecode(response.body);
+    data.addAll(responsex);
+    print(responsex);
+    loading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    api();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,29 +37,6 @@ class _MyState extends State<Home> {
       ),
       body: ListView(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            height: 40,
-            margin: EdgeInsets.symmetric(horizontal: 100, vertical: 10),
-            child: MaterialButton(
-              onPressed: () async {
-                loading = true;
-                setState(() {});
-                var response = await get(
-                  Uri.parse("https://jsonplaceholder.org/posts"),
-                );
-                var responsex = jsonDecode(response.body);
-                data.addAll(responsex);
-                print(responsex);
-                loading = false;
-                setState(() {});
-              },
-              child: Text("request", style: TextStyle(color: Colors.white)),
-            ),
-          ),
           if (loading) Center(child: CircularProgressIndicator()),
           ...List.generate(
             data.length,
